ae7cbae34d4dd6946f282939c5711017a92e81ce
KALKIDAN-SEMRE <kalkidan.semre1221@gmail.com>
2025-11-03 03:10:55 +0300

    resolve configuration issue in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
@@ -11,10 +9,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
   optimizeDeps: {
     exclude: ["pdfjs-dist"],
   },
-}));
+});
